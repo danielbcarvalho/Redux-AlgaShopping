0f7fc273aa5b987b6a425064ebf30cae1fa9a68a
danielbcarvalho <danielborgesdecarvalho@gmail.com>
2021-05-16 19:43:42 -0300

    Redux working with Calculator exemple

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { sum } from "../store/Calculator/Calculator.actions";
+
+function Calculator() {
+  const dispatch = useDispatch();
+  const result = useSelector((state) => state.calculator);
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+  //dispatch(sum);
+
+  return (
+    <>
+      <input
+        type="number"
+        placeholder="a"
+        value={a}
+        onChange={(e) => setA(Number(e.target.value))}
+      />
+      <input
+        type="number"
+        placeholder="b"
+        value={b}
+        onChange={(e) => setB(Number(e.target.value))}
+      />
+      <button
+        onClick={() => {
+          dispatch(sum(a, b));
+        }}
+      >
+        Somar
+      </button>
+      <button>Subtrair</button>
+      <div>Resultado = {result}</div>
+    </>
+  );
+}
+
+export default Calculator;
