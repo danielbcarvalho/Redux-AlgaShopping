91f4f436c61dfa3211dd204bfb936fc997467a7d
danielbcarvalho <danielborgesdecarvalho@gmail.com>
2021-05-16 21:12:30 -0300

    Refactored with redux

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,33 +7,24 @@ import { Wrapper, Container } from "./App.styles";
 import productsMock from "../../mocks/products.json";
 import extractPercentage from "../../utils/extractPercentage";
 import Calculator from "../Calculator";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectAllProducts,
+  selectSelectedProducts,
+  selectSelectedTotalProductsPrice,
+} from "../../store/Products/Products.selectors";
+import { toggleProduct } from "../../store/Products/Products.actions";
 
 function App() {
   const colors = ["#62CBC6", "#00ABAD", "#00858C", "#006073", "#004D61"];
+  const dispatch = useDispatch();
 
-  const [products, setProducts] = useState(productsMock.products);
-  const [selectedProducts, setSelectedProducts] = useState([]);
-  const [totalPrice, setTotalPrice] = useState(0);
+  const products = useSelector(selectAllProducts);
+  const selectedProducts = useSelector(selectSelectedProducts);
+  const totalPrice = useSelector(selectSelectedTotalProductsPrice);
 
-  useEffect(() => {
-    const newSelectedProducts = products.filter((product) => product.checked);
-
-    setSelectedProducts(newSelectedProducts);
-  }, [products]);
-
-  useEffect(() => {
-    const total = selectedProducts
-      .map((product) => product.price)
-      .reduce((a, b) => a + b, 0);
-
-    setTotalPrice(total);
-  }, [selectedProducts]);
-
-  function handleToggle(id, checked, name) {
-    const newProducts = products.map((product) =>
-      product.id === id ? { ...product, checked: !product.checked } : product
-    );
-    setProducts(newProducts);
+  function handleToggle(id) {
+    dispatch(toggleProduct(id));
   }
 
   return (
@@ -44,15 +35,14 @@ function App() {
           left={
             <ShoppingList
               title="Produtos disponíveis"
-              products={products}
               onToggle={handleToggle}
             />
           }
           middle={
             <ShoppingList
               title="Sua lista de compras"
-              products={selectedProducts}
               onToggle={handleToggle}
+              displayOnlySelected
             />
           }
           right={
